d3e2988ae9da286448e34a5471f5efa14de7fcc2
Piotr Świderski <piotr.swiderski@b2net.pl>
2019-12-24 11:32:47 +0100

    database connection cfg

---
+++ config/Database.php
@@ -0,0 +1,47 @@
+<?php
+
+
+/**
+ * Database configuration options
+ */
+class Database
+{
+
+    /**
+     * Db parameters
+     */
+    private $host = 'localhost';
+    private $dbName = 'RestApiNF';
+    private $user = 'root';
+    private $password = 'root';
+    private $connection;
+
+    /**
+     * Db Connection
+     */
+    public function connect()
+    {
+        $this->connection = null;
+
+        try {
+            $this->connection = new PDO(
+                'mysql:host=' . $this->host .
+                ';dbname=' . $this->dbName,
+                $this->user,
+                $this->password
+            );
+
+            /**
+             * Exceptions information
+             */
+            $this->connection->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+        } catch (PDOException $e) {
+            echo 'Connection error: ' . $e->getMessage();
+        }
+
+        return $this->connection;
+    }
+}
